5cd78d4503395fec1fc68e96bd5effc8c960ce3a
Amador Pahim <apahim@redhat.com>
2020-02-18 18:29:18 +0000

    Updating setup.py
    Including more information.
    
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- setup.py
+++ setup.py
@@ -1,11 +1,40 @@
+import os
+
 from setuptools import setup, find_packages
+
+
+BASE_PATH = os.path.dirname(__file__)
+
+
+def get_readme_content():
+    with open(os.path.join(BASE_PATH, 'README.md'), 'r') as readme:
+        return readme.read()
+
+
+def get_version():
+    with open(os.path.join(BASE_PATH, 'VERSION'), 'r') as version:
+        return version.read().strip()
 
 
 setup(name='sretoolbox',
       packages=find_packages(),
-      version=open('VERSION').read().strip(),
+      version=get_version(),
       author='Red Hat Application SRE Team',
       author_email="sd-app-sre@redhat.com",
+      url='https://github.com/app-sre/sretoolbox',
+      description='Set of libraries commonly used by multiple SRE projects',
+      long_description=get_readme_content(),
       python_requires='>=3.6',
       license="GPLv2+",
+      classifiers=
+      [
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU General Public License v2 or '
+            'later (GPLv2+)',
+            'Natural Language :: English',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Software Development :: Libraries',
+      ],
       install_requires=['requests~=2.22'])
