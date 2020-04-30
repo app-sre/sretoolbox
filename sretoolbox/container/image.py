00d288c0243a5b4efdfc924bd7ea1504ff176078
Amador Pahim <apahim@redhat.com>
2020-04-30 11:40:19 +0100

    Sort methods
    Sorted per type:
    
    - public
    - private
    - dunder
    
    Then sorted alphabetically.
    
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- sretoolbox/container/image.py
+++ sretoolbox/container/image.py
@@ -58,144 +58,6 @@ class Image:
 
         self._cache_tags = None
 
-    @property
-    def _tags(self):
-        if self._cache_tags is None:
-            try:
-                self._cache_tags = self.get_tags()
-            except requests.exceptions.HTTPError:
-                self._cache_tags = []
-
-        return self._cache_tags
-
-    def __eq__(self, other):
-        # Two instances are considered equal if both of their
-        # manifests are accessible and first item of the 'history'
-        # (the most recent) is the same.
-        try:
-            manifest = self.get_manifest()
-            other_manifest = other.get_manifest()
-        except ImageManifestError as details:
-            raise ImageComparisonError(details)
-
-        if (manifest['history'][0]['v1Compatibility'] ==
-                other_manifest['history'][0]['v1Compatibility']):
-            return True
-
-        return False
-
-    def __getitem__(self, item):
-        return Image(url=str(self), tag_override=str(item))
-
-    def __iter__(self):
-        for tag in self._tags:
-            yield tag
-
-    def __len__(self):
-        return len(self._tags)
-
-    def __contains__(self, item):
-        return item in self._tags
-
-    def __str__(self):
-        return (f'{self.scheme}'
-                f'{self.registry}'
-                f'/{self.repository}'
-                f'/{self.image}'
-                f':{self.tag}')
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}(url='{self}')"
-
-    def __bool__(self):
-        try:
-            self.get_manifest()
-            return True
-        except ImageManifestError:
-            return False
-
-    def _raise_for_status(self, response, error_msg=None):
-        """
-        Includes the error messages, important for a registry
-        """
-        if response.status_code < 400:
-            return None
-
-        msg = ''
-        if error_msg is not None:
-            msg += f'{error_msg}: '
-
-        msg += f'({response.status_code}) {response.reason}'
-        content = json.loads(response.content)
-        if "errors" in content:
-            for error in content['errors']:
-                msg += f', {error["message"]}'
-        _LOG.error('[%s, %s]', str(self), msg)
-        raise requests.exceptions.HTTPError(msg)
-
-    def _get_auth(self, www_auth):
-        """
-        Generates the authorization string.
-        """
-        scheme = www_auth.pop("scheme")
-
-        url = f'{www_auth.pop("realm")}?'
-        for key, value in www_auth.items():
-            url += f'{key}={value}&'
-
-        if all([self.username is not None,
-                self.password is not None]):
-            auth = (self.username, self.password)
-        else:
-            auth = None
-
-        response = requests.get(url, auth=auth)
-
-        if response.status_code == 401:
-            # Try again without auth
-            response = requests.get(url)
-
-        self._raise_for_status(response, error_msg=f'unable to retrieve auth '
-                                                   f'token from {url}')
-
-        data = response.json()["token"]
-        return f'{scheme} {data}'
-
-    @staticmethod
-    def _parse_www_auth(value):
-        www_authenticate = dict()
-        www_authenticate['scheme'], params = value.split(' ', 1)
-
-        # According to the RFC6750, the scheme MUST be followed by
-        # one or more auth-param values.
-        # This regex gets the extra auth-params and adds them to
-        # the www_authenticate dictionary
-        for item in re.finditer('(?P<key>[^ ,]+)="(?P<value>[^"]+)"', params):
-            www_authenticate[item.group('key')] = item.group('value')
-
-        return www_authenticate
-
-    def _request_get(self, url):
-        # Try first without 'Authorization' header
-        headers = {
-            'Accept': 'application/vnd.docker.distribution.manifest.v1+json'
-        }
-        response = requests.get(url, headers=headers)
-
-        # Unauthorized, meaning we have to acquire a token
-        if response.status_code == 401:
-            auth_specs = response.headers.get('Www-Authenticate')
-            if auth_specs is None:
-                self._raise_for_status(response)
-            www_auth = self._parse_www_auth(auth_specs)
-
-            # Try again, this time with the Authorization header
-            headers['Authorization'] = self._get_auth(www_auth)
-            response = requests.get(url, headers=headers)
-
-        self._raise_for_status(response)
-        return response
-
     def get_tags(self):
         """
         Goes to the internet to retrieve all the image tags.
@@ -237,6 +99,29 @@ class Image:
         except (requests.exceptions.HTTPError,
                 json.decoder.JSONDecodeError):
             raise ImageManifestError(f"{self} can't access image manifest")
+
+    def _get_auth(self, www_auth):
+        """
+        Generates the authorization string.
+        """
+        scheme = www_auth.pop("scheme")
+
+        url = f'{www_auth.pop("realm")}?'
+        for key, value in www_auth.items():
+            url += f'{key}={value}&'
+
+        if all([self.username is not None,
+                self.password is not None]):
+            auth = (self.username, self.password)
+        else:
+            auth = None
+
+        response = requests.get(url, auth=auth)
+        self._raise_for_status(response, error_msg=f'unable to retrieve auth '
+                                                   f'token from {url}')
+
+        data = response.json()["token"]
+        return f'{scheme} {data}'
 
     @staticmethod
     def _parse_image_url(image_url):
@@ -311,3 +196,113 @@ class Image:
             image_url_struct['tag'] = default_tag
 
         return image_url_struct
+
+    @staticmethod
+    def _parse_www_auth(value):
+        www_authenticate = dict()
+        www_authenticate['scheme'], params = value.split(' ', 1)
+
+        # According to the RFC6750, the scheme MUST be followed by
+        # one or more auth-param values.
+        # This regex gets the extra auth-params and adds them to
+        # the www_authenticate dictionary
+        for item in re.finditer('(?P<key>[^ ,]+)="(?P<value>[^"]+)"', params):
+            www_authenticate[item.group('key')] = item.group('value')
+
+        return www_authenticate
+
+    def _request_get(self, url):
+        # Try first without 'Authorization' header
+        headers = {
+            'Accept': 'application/vnd.docker.distribution.manifest.v1+json'
+        }
+        response = requests.get(url, headers=headers)
+
+        # Unauthorized, meaning we have to acquire a token
+        if response.status_code == 401:
+            auth_specs = response.headers.get('Www-Authenticate')
+            if auth_specs is None:
+                self._raise_for_status(response)
+            www_auth = self._parse_www_auth(auth_specs)
+
+            # Try again, this time with the Authorization header
+            headers['Authorization'] = self._get_auth(www_auth)
+            response = requests.get(url, headers=headers)
+
+        self._raise_for_status(response)
+        return response
+
+    def _raise_for_status(self, response, error_msg=None):
+        """
+        Includes the error messages, important for a registry
+        """
+        if response.status_code < 400:
+            return None
+
+        msg = ''
+        if error_msg is not None:
+            msg += f'{error_msg}: '
+
+        msg += f'({response.status_code}) {response.reason}'
+        content = json.loads(response.content)
+        if "errors" in content:
+            for error in content['errors']:
+                msg += f', {error["message"]}'
+        _LOG.error('[%s, %s]', str(self), msg)
+        raise requests.exceptions.HTTPError(msg)
+
+    @property
+    def _tags(self):
+        if self._cache_tags is None:
+            try:
+                self._cache_tags = self.get_tags()
+            except requests.exceptions.HTTPError:
+                self._cache_tags = []
+
+        return self._cache_tags
+
+    def __eq__(self, other):
+        # Two instances are considered equal if both of their
+        # manifests are accessible and first item of the 'history'
+        # (the most recent) is the same.
+        try:
+            manifest = self.get_manifest()
+            other_manifest = other.get_manifest()
+        except ImageManifestError as details:
+            raise ImageComparisonError(details)
+
+        if (manifest['history'][0]['v1Compatibility'] ==
+                other_manifest['history'][0]['v1Compatibility']):
+            return True
+
+        return False
+
+    def __getitem__(self, item):
+        return Image(url=str(self), tag_override=str(item))
+
+    def __iter__(self):
+        for tag in self._tags:
+            yield tag
+
+    def __len__(self):
+        return len(self._tags)
+
+    def __contains__(self, item):
+        return item in self._tags
+
+    def __str__(self):
+        return (f'{self.scheme}'
+                f'{self.registry}'
+                f'/{self.repository}'
+                f'/{self.image}'
+                f':{self.tag}')
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(url='{self}')"
+
+    def __bool__(self):
+        try:
+            self.get_manifest()
+            return True
+        except ImageManifestError:
+            return False
