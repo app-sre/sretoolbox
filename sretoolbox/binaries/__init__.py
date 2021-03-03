b402c24a541be620d648f7ea0eb97f1079234e54
Amador Pahim <apahim@redhat.com>
2021-03-03 09:38:17 +0000

    Adding operator-sdk binary class
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- sretoolbox/binaries/__init__.py
+++ sretoolbox/binaries/__init__.py
@@ -3,8 +3,10 @@ Exposes the binary libs for easy access.
 """
 
 from sretoolbox.binaries.opm import Opm
+from sretoolbox.binaries.operator_sdk import OperatorSDK
 
 
 __all__ = [
     'Opm',
+    'OperatorSDK',
 ]
