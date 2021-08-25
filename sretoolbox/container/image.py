64a2b5c2d0d70ba9f85ff27fbdc8a22fc956fbe9
Luis Fernando Muñoz Mejías <lmunozme@redhat.com>
2021-08-26 00:08:10 +0200

    Cache authorization tokens in Image
    Right now, every GET request to at least dockerhub triggers an
    authentication failure and a cycle of re-authentication. If, however,
    we keep the received token, we can save several round trips reduce
    load in the registry.
    
    This speeds up executions considerably. For the following silly
    script:
    
    ```python
    from sretoolbox.container import Image
    import time
    
    n = time.time()
    
    A_SHA = 'sha256:bc1ed82a75f2ca160225b8281c50b7074e7678c2a1f61b1fb298e545b455925e'
    i = Image(
        'docker://docker.io/library/memcached:latest',
        response_cache={},
        username='MYUSER', password='MYPASSWORD'
    )
    
    print(i)
    print(i.url_digest)
    print(i.manifest)
    print(i.url_tag)
    print(len(i.response_cache))
    print(i.tags)
    print(time.time()-n)
    ```
    
    the timing goes from 12.6 seconds on current master to 5.3 with these
    changes.
    
    Fixes #44

--- sretoolbox/container/image.py
+++ sretoolbox/container/image.py
@@ -41,6 +41,7 @@ class NoTagForImageByDigest(Exception):
     Raised when the Image was constructed with a by-digest URL and an
     operation is attempted that requires a tag.
     """
+
     def __init__(self, image):
         super().__init__(
             f"Can't determine a unique tag for Image: {str(image)}")
@@ -73,6 +74,7 @@ class Image:
         self.image = image_data['image']
         self.response_cache = response_cache
 
+        self._auth_token = None
         if tag_override is None:
             self.tag = image_data['tag']
         else:
@@ -361,17 +363,23 @@ class Image:
 
     @retry(exceptions=(HTTPError, requests.ConnectionError), max_attempts=5)
     def _request_get(self, url, method=requests.get):
-        # Try first without 'Authorization' header
+        # Use any cached tokens, they may still be valid
         headers = {
             'Accept':
-                'application/vnd.docker.distribution.manifest.v1+json,'
-                'application/vnd.docker.distribution.manifest.v2+json,'
-                'application/vnd.docker.distribution.manifest.v1+prettyjws,'
+            'application/vnd.docker.distribution.manifest.v1+json,'
+            'application/vnd.docker.distribution.manifest.v2+json,'
+            'application/vnd.docker.distribution.manifest.v1+prettyjws,'
         }
 
-        response = method(url, headers=headers, auth=self.auth)
+        if self._auth_token:
+            headers['Authorization'] = self._auth_token
+            auth = None
+        else:
+            auth = self.auth
 
-        # Unauthorized, meaning we have to acquire a token
+        response = method(url, headers=headers, auth=auth)
+
+        # Unauthorized, meaning we have to acquire a new token
         if response.status_code == 401:
             auth_specs = response.headers.get('Www-Authenticate')
             if auth_specs is None:
@@ -379,8 +387,9 @@ class Image:
 
             www_auth = self._parse_www_auth(auth_specs)
 
-            # Try again, this time with the Authorization header
-            headers['Authorization'] = self._get_auth(www_auth)
+            # Try again, with the new Authorization header
+            self._auth_token = self._get_auth(www_auth)
+            headers['Authorization'] = self._auth_token
             response = method(url, headers=headers)
 
         self._raise_for_status(response)
