3dbcef80f4a8b3153004cb5bf76771bf37f87cb4
Maor Friedman <maor.friedman@redhat.com>
2021-11-11 15:02:46 +0200

    add threaded functions to init

--- sretoolbox/utils/__init__.py
+++ sretoolbox/utils/__init__.py
@@ -19,10 +19,12 @@ Exposes the utilities for easy access.
 from sretoolbox.utils.datatransformation import replace_values
 from sretoolbox.utils.retry import retry
 from sretoolbox.utils.process import run
+from sretoolbox.utils.retry import run, estimate_available_thread_pool_size
 
 
 __all__ = [
     'replace_values',
     'retry',
     'run',
+    'estimate_available_thread_pool_size',
 ]
