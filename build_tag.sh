c40e03933cdc39e12def99be3be56ef606994bfe
Christian Assing <cassing@redhat.com>
2024-12-02 13:58:32 +0100

    fix pypi publish

--- build_tag.sh
+++ build_tag.sh
@@ -2,6 +2,6 @@
 
 docker build -t sretoolbox -f Dockerfile \
     --target pypi \
-    --build-arg UV_PUBLISH_USERNAME="$TWINE_USERNAME" \
-    --build-arg UV_PUBLISH_PASSWORD="$TWINE_PASSWORD" \
+    --build-arg TWINE_USERNAME \
+    --build-arg TWINE_PASSWORD \
     .
